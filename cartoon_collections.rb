e04fc3b62d137d06ae74f5ccac67b48cb506fadc
Kierra M <k.merriman32@hotmail.com>
2019-08-28 15:42:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,6 @@ def find_the_cheese# (cheddar_cheese)
   #cheese_types = ["cheddar", "gouda", "camembert"]
   return find_the_cheese.include? ("cheddar","gouda","camembert")
 end
-end
 
 #to install gem write " gem install pry" in terminal then require 'pry'
  #at top of file in text editor
