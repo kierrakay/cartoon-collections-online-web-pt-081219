a1084cf9505d8c41a2740f4256acfe0b2f0343f3
Kierra M <k.merriman32@hotmail.com>
2019-08-28 16:41:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
 cheddar_cheese.each do |cheese|
   if cheddar_cheese.include? cheese_types
-  cheese
+  cheese_types
   end
   end
 return nil
