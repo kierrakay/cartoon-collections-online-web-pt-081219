d5b2e7476d76057f5bcc42257b40de0949eae255
Kierra M <k.merriman32@hotmail.com>
2019-08-28 13:32:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
  # planeteer_calls.collect!.with_index {|x, i| x[0...i] }
-   planeteer_calls.collect {|x| x + "!"} 
+   planeteer_calls.collect {|x| x + "!"+ 1} 
 end
 
 def long_planeteer_calls# code an argument here
