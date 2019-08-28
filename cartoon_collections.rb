9dda8020393cba28f9195717204d37eb152dd3c0
Kierra M <k.merriman32@hotmail.com>
2019-08-28 14:06:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  if calls_long.validates_length_of < 4
+  if calls_long.validates_length_of > 4
     true 
   else
     false
