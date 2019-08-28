c189597ae2f96a53554d705e86644a99e4240a07
Kierra M <k.merriman32@hotmail.com>
2019-08-28 16:58:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end #this is the method end
 
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-cheddar_cheese.each do |cheese| #that is the variable we are using to represent a single element in the iteration...what are we using to reference each element in the cheddar_cheese array? |cheese| <-- this variable between pipes
+cheddar_cheese.each do |cheese| #what are we using to reference each element in the cheddar_cheese array? |cheese| <-- this variable between pipes...that is the variable we are using to represent a single element in the iteration...
   if cheese_types.include? cheese
   return cheese
   end
