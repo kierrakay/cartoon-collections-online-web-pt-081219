b95b06bf78e6eea8c6eda8c4f8e0d2c184b89c23
Kierra M <k.merriman32@hotmail.com>
2019-08-28 15:53:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -41,4 +41,5 @@ end
 
 
 
+
 #notes: don't put "puts" on line 9 because it'll return nil when we want to return the strings. .each returns original array and .map/.collect returns new array. .collect returns a new array but doesn't actually change what's stored in planeteer calls.if you have planeteer_calls on the last line of your method you are returning that value, rather than leveraging what .collect returns
