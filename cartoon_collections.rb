dbb4057361bcfd0f7720a7d8803c9f742d3e04d5
Kierra M <k.merriman32@hotmail.com>
2019-08-28 13:56:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)
      planeteer_calls.map do |i|
       puts i.capitalize + "!"
   end
-end
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
